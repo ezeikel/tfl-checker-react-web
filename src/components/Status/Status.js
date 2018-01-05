9583cd98e53b185f59fe9e23c69ce433894f4e92
Ezeikel Pemberton <ezeikelpemberton@gmail.com>
2018-01-05 00:28:29 +0000

    Added call to tfl api. Test data is being rendered in status component.

--- src/components/Status/Status.js
+++ src/components/Status/Status.js
@@ -1,9 +1,28 @@
 import React, { Component } from 'react';
 
 class Status extends Component {
+    state = {
+        status: {}
+    }
+
+    async componentWillMount() {
+        const data = await fetch('https://api.tfl.gov.uk/line/mode/tube/status');
+        const json = await data.json();
+        this.setState({status: json});
+    }
+
     render() {
+        console.log(this.state.status);
+        this.st
         return (
-            <div>Status component works!</div>
+            <div>
+                <div>Status component works!</div>
+                {
+                    Object
+                        .keys(this.state.status)
+                        .map(line => <div key={this.state.status[line].id}><pre>{this.state.status[line].name}</pre></div>)
+                }
+            </div>
         )
     }
 };
