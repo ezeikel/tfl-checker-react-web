928fa6ede8af29959fb9256b20de36afed5f22f8
Ezeikel Pemberton <ezeikelpemberton@gmail.com>
2018-04-12 23:12:22 +0100

    Two spaces.

--- src/components/Planner/Planner.js
+++ src/components/Planner/Planner.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 
 class Planner extends Component {
-    render() {
-        return (
-            <div>Planner component works!</div>
-        )
-    }
+  render() {
+    return (
+      <div>Planner component works!</div>
+    )
+  }
 };
 
 export default Planner;
--- src/components/Status/Status.js
+++ src/components/Status/Status.js
@@ -5,30 +5,30 @@ import "./Status.css";
 import LineStatus from "./LineStatus/LineStatus";
 
 class Status extends Component {
-    state = {
-        status: {}
-    }
+  state = {
+    status: {}
+  }
 
-    async componentWillMount() {
-        const data = await fetch('https://api.tfl.gov.uk/line/mode/tube/status');
-        const json = await data.json();
-        this.setState({status: json});
-    }
+  async componentWillMount() {
+    const data = await fetch('https://api.tfl.gov.uk/line/mode/tube/status');
+    const json = await data.json();
+    this.setState({status: json});
+  }
 
-    render() {
-        return (
-            <div>
-                <h4>Status:</h4>
-                <ul className="tfl-lines">
-                    {
-                        Object
-                            .keys(this.state.status)
-                            .map(line => <LineStatus key={this.state.status[line].id} line={this.state.status[line]} />)
-                    }
-                </ul>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <h4>Status:</h4>
+        <ul className="tfl-lines">
+          {
+            Object
+              .keys(this.state.status)
+              .map(line => <LineStatus key={this.state.status[line].id} line={this.state.status[line]} />)
+          }
+        </ul>
+      </div>
+    )
+  }
 };
 
 export default Status;
