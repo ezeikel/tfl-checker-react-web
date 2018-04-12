ed7e65e53cc317c5a825b5ac14608706ce899f78
Ezeikel Pemberton <ezeikelpemberton@gmail.com>
2018-04-13 00:40:53 +0100

    Refactoring Status component to use styled components. WIP.

--- src/components/Status/Status.js
+++ src/components/Status/Status.js
@@ -1,32 +1,46 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+import TubeLine from "./TubeLine/TubeLine";
 
-import "./Status.css";
+const Lines = styled.ul`
+  display: flex;
+  flex-direction: column;
+`;
 
-import LineStatus from "./LineStatus/LineStatus";
+const Title = styled.h4`
+  font-size: 18px;
+  margin-top: 10px;
+  margin-bottom: 10px;
+`;
 
 class Status extends Component {
   state = {
-    status: {}
-  }
+    status: []
+  };
 
-  async componentWillMount() {
+  async componentDidMount() {
     const data = await fetch('https://api.tfl.gov.uk/line/mode/tube/status');
     const json = await data.json();
-    this.setState({status: json});
+
+    this.setState({
+      status: json
+    });
   }
+
+  renderLines = () => (
+    this.state.status.map(line => (
+      <TubeLine key={line.id} line={line} />
+    ))
+  );
 
   render() {
     return (
-      <div>
-        <h4>Status:</h4>
-        <ul className="tfl-lines">
-          {
-            Object
-              .keys(this.state.status)
-              .map(line => <LineStatus key={this.state.status[line].id} line={this.state.status[line]} />)
-          }
-        </ul>
-      </div>
+      <React.Fragment>
+        <Title>Status:</Title>
+        <Lines>
+          {this.renderLines()}
+        </Lines>
+      </React.Fragment>
     )
   }
 };
