d6c9d9364b17c50d366df24600c9c52836456759
Ezeikel Pemberton <ezeikelpemberton@gmail.com>
2020-03-15 13:13:51 +0000

    Pulling relevant data out of journey planner api call and displaying in component.

--- src/components/JourneyResults.js
+++ src/components/JourneyResults.js
@@ -1,12 +1,60 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.section`
+  background-color: #fff;
+`;
+
+const Journey = styled.div`
+  margin-bottom: 32px;
+`;
+
+const Leg = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Steps = styled.div`
+  margin: 16px;
+`;
+
+const Step = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 
 const JourneyResults = ({ results }) => {
     console.log({ results });
-    return (
-      <div>
-        Journey results.
+    const { journeys } = results;
 
-      </div>
+    if (!journeys) return <div>Empty.</div>
+    return (
+      <Wrapper>
+        {
+          journeys.map((journey, i) => (
+            <Journey key={i}>
+              <span>Duration: {journey.duration}</span>
+              {
+                journey.legs.map((leg, i) => (
+                  <Leg key={i}>
+                    <span>Duration: {leg.duration}</span>
+                    <span>{leg.instruction.summary}</span>
+                    <Steps className="steps">
+                      {leg.instruction.steps.map((step, i) => (
+                        <Step className="step" key={i}>
+                          <span>{step.descriptionHeading}</span>
+                          <span>{step.description}</span>
+                        </Step>
+                      ))}
+                    </Steps>
+                  </Leg>
+                ))
+              }
+            </Journey>
+          ))
+
+        }
+      </Wrapper>
     );
 };
 
--- src/components/Planner.js
+++ src/components/Planner.js
@@ -1,6 +1,11 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
 import GooglePlacesInput from './GooglePlacesInput';
 import JourneyResults from './JourneyResults';
+
+const Wrapper = styled.section`
+  background-color: #ccdde8;
+`;
 
 const Planner = () => {
   const API_ENDPOINT = "https://api.tfl.gov.uk/"
@@ -14,7 +19,7 @@ const Planner = () => {
     lng: null
   });
 
-  const [journeyResults, setJourneyResults] = useState({});
+  const [journeyResults, setJourneyResults] = useState();
 
   const handleSubmit = async () => {
     const resultsJson = await fetch(`${API_ENDPOINT}journey/journeyresults/${fromCoordinates.lat},${fromCoordinates.lng}/to/${toCoordinates.lat},${toCoordinates.lng}?app_id=1b83c22c&app_key=e5c7b582d0f72a04add248393e939cf5`);
@@ -23,12 +28,14 @@ const Planner = () => {
   };
 
   return (
-    <div>
+    <Wrapper>
       <GooglePlacesInput coordinates={fromCoordinates} setCoordinates={setFromCoordinates} />
       <GooglePlacesInput coordinates={toCoordinates} setCoordinates={setToCoordinates} />
       <button onClick={handleSubmit}>Submit</button>
-      <JourneyResults results={journeyResults} />
-    </div>
+      {
+        journeyResults && <JourneyResults results={journeyResults} />
+      }
+    </Wrapper>
   );
 };
 
