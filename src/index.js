036377fc33b08692323053c22c075d0b8140f989
Ezeikel Pemberton <ezeikelpemberton@gmail.com>
2020-03-20 11:51:43 +0000

    Removed gh pages script.

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
 const app = (
-    <Router basename={`${process.env.PUBLIC_URL}/`}>
+    <Router>
         <App />
     </Router>
 );
