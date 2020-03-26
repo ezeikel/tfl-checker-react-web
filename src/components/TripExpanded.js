51d3b5ef2923e253d2cf657947bacc9d6ffa08d7
Ezeikel Pemberton <ezeikelpemberton@gmail.com>
2020-03-26 12:52:33 +0000

    More styling from TripExpanded. WIP.

--- src/components/TripExpanded.js
+++ src/components/TripExpanded.js
@@ -3,6 +3,29 @@ import styled from 'styled-components';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import convertDateToTime from "../utils/convertDateToTime";
 
+const MODE_COLOURS = {
+  walking: {
+    icon: "walking",
+    color: "#4D4D4D"
+  },
+  bus: {
+    icon: "bus",
+    color: "#C83638"
+  },
+  "national-rail": {
+    icon: "train",
+    color: "#043261"
+  },
+  overground: {
+    icon: "train",
+    color: "#E46B24"
+  },
+  tube: {
+    icon: "subway",
+    color: "#051EA6"
+  }
+};
+
 const Wrapper = styled.section`
   display: flex;
   flex-direction: column;
@@ -20,9 +43,12 @@ const Leg = styled.section`
 const Header = styled.section`
   display: flex;
   align-items: center;
-  /* svg {
-    margin-right: 16px;
-  } */
+  margin-bottom: 8px;
+`;
+
+const Footer = styled.section`
+  display: flex;
+  align-items: center;
 `;
 
 const Content = styled.section`
@@ -31,33 +57,78 @@ const Content = styled.section`
 
 const Point = styled.span`
   font-size: 2rem;
+  margin-left: 4px;
 `;
 
 const Stops = styled.div`
   display: flex;
   flex-direction: column;
   font-size: 1.6rem;
+  > div {
+    & + div {
+      margin-top: 16px;
+    }
+    &:last-of-type {
+      margin-bottom: 16px;
+    }
+  }
 `;
 
 const Stop = styled.div`
   display: flex;
   flex-direction: column;
+  position: relative;
+  &:before {
+    content: "";
+    display: block;
+    height: 4px;
+    width: 12px;
+    border-radius: 4px;
+    background-color: #4D4D4D;
+    background-color: ${({ color }) => color };
+    position: absolute;
+    top: 50%;
+    left: -4px;
+    margin-top: -2px;
+    margin-left: -15px;
+  }
 `;
 
 const Direction = styled.div`
   display: flex;
   flex-direction: column;
+  display: relative;
+  position: relative;
+  &:before {
+    content: "";
+    display: block;
+    height: 4px;
+    width: 12px;
+    border-radius: 4px;
+    background-color: #4D4D4D;
+    position: absolute;
+    top: 50%;
+    left: -4px;
+    margin-top: -2px;
+    margin-left: -15px;
+  }
+  span {
+    &:first-child {
+      font-size: 18px;
+    }
+  }
 `;
 
 const Summary = styled.div`
   display: flex;
   flex-direction: column;
   font-size: 1.8rem;
+  margin-bottom: 8px;
 `;
 
 const VerticalLine = styled.div`
   display: flex;
-  background-color: #4D4D4D;
+  background-color: ${({ color }) => color };
   width: 4px;
   min-height: 32px;
   border-radius: 4px;
@@ -68,6 +139,7 @@ const VerticalLine = styled.div`
 const Duration = styled.div`
   display: flex;
   font-size: 1.6rem;
+  margin-bottom: 8px;
   span {
     &:first-of-type {
       font-weight: bold;
@@ -103,15 +175,15 @@ const Trip = ({ trip }) => {
             <Header>
               <ModeIcon>
                 <FontAwesomeIcon
-                  icon={["fad", leg.mode.id]}
-                  color="var(--color-dark-grey)"
+                  icon={["fad", MODE_COLOURS[leg.mode.id].icon]}
+                  color={MODE_COLOURS[leg.mode.id].color}
                   size="2x"
                 />
               </ModeIcon>
               <Point className="location-name">{`${leg.departurePoint.commonName} at ${convertDateToTime(leg.departureTime)}`}</Point>
             </Header>
             <Content>
-              <VerticalLine />
+              <VerticalLine color={MODE_COLOURS[leg.mode.id].color} />
               <div>
                 <Summary>{leg.instruction.summary}</Summary>
                 <Duration>
@@ -134,7 +206,7 @@ const Trip = ({ trip }) => {
                   }
                   {(leg.mode.id === "bus" || leg.mode.id === "tube") &&
                     leg.path.stopPoints.map((stopPoint, i) => (
-                      <Stop key={i}>
+                      <Stop key={i} color={MODE_COLOURS[leg.mode.id].color}>
                         <span>{stopPoint.name}</span>
                       </Stop>
                     ))
@@ -145,18 +217,16 @@ const Trip = ({ trip }) => {
           </Leg>
         ))
       }
-      <div>
-        <div className="summary">
-          <ModeIcon>
-            <FontAwesomeIcon
-              icon={["fad", "map-marker-alt"]}
-              color="var(--color-dark-grey)"
-              size="3x"
-            />
-          </ModeIcon>
-          <span>{trip.legs[trip.legs.length -1].arrivalPoint.commonName}</span>
-        </div>
-      </div>
+      <Footer>
+        <ModeIcon>
+          <FontAwesomeIcon
+            icon={["fad", "map-marker-alt"]}
+            color="var(--color-dark-grey)"
+            size="2x"
+          />
+        </ModeIcon>
+        <Point className="location-name">{`${trip.legs[trip.legs.length -1].arrivalPoint.commonName} at ${convertDateToTime(trip.legs[trip.legs.length -1].departureTime)}`}</Point>
+      </Footer>
     </Wrapper>
   );
 };
