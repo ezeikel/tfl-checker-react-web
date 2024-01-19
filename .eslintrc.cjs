4620967c2e8653f3d4ecd449625ca589eee2890b
Ezeikel Pemberton <hi@ezeikel.dev>
2024-01-19 22:28:07 +0000

    fix: vercel.json file to fix 404 on page refresh.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,43 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react-hooks/recommended",
-  ],
+  extends: ["next", "airbnb", "plugin:prettier/recommended"],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
-  parser: "@typescript-eslint/parser",
   plugins: ["react-refresh"],
   rules: {
-    "react-refresh/only-export-components": [
-      "warn",
-      { allowConstantExport: true },
-    ],
+    "react/react-in-jsx-scope": 0,
+    "react/state-in-constructor": 0,
+    "react/function-component-definition": 0,
+    "react/prop-types": 0,
+    "react/require-default-props": 0,
+    "no-console": [2, { allow: ["warn", "error"] }],
   },
+  overrides: [
+    {
+      files: "**/*.ts?(x)",
+      parser: "@typescript-eslint/parser",
+      parserOptions: {
+        project: "./tsconfig.json",
+      },
+      plugins: ["@typescript-eslint/eslint-plugin"],
+      extends: ["airbnb-typescript", "plugin:prettier/recommended"],
+      rules: {
+        "import/no-extraneous-dependencies": [
+          "error",
+          {
+            devDependencies: [
+              "./jest.config.ts",
+              "./test/jest.common.ts",
+              "./test/jest.client.ts",
+              "./test/jest.lint.ts",
+              "./test/jest.setup.ts",
+              "./lib/utils.ts",
+              "**/*.test.ts",
+              "**/*.test.tsx",
+            ],
+          },
+        ],
+      },
+    },
+  ],
 };
